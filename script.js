4a9a62483a3f5ef14794ca984dd48fe9975a61c1
Kaitlyn Kim <kk4874a@student.american.edu>
2023-09-24 23:54:10 -0400

    initial js set up

---
+++ script.js
@@ -0,0 +1,8 @@
+//search html
+const cityInput = document.querySelector(".weather-input");
+const searchButton = document.querySelector(".search-btn");
+const currentWeatherDiv = document.querySelector(".current-weather-details");
+const weatherCardsDiv = document.querySelector(".forecast-cards");
+// API key for OpenWeatherMap API
+const API_KEY = "73ee450a2ec37d63bb0d211cc2ec1999";
+
